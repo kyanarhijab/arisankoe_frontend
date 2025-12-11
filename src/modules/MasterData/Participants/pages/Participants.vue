4a51378f6ac7f3e81a0f1e9938a88fdcaca9fa95
nodp <nodp@ubslinux.com>
2025-12-11 23:09:03 +0700

    update

--- src/modules/MasterData/Participants/pages/Participants.vue
+++ src/modules/MasterData/Participants/pages/Participants.vue
@@ -1,13 +1,14 @@
 <script setup>
 import BaseModalForm from '@/components/BaseModalForm.vue'
-import ParticipantsForm from '@/modules/MasterData/Participants/components/ParticipantsForm.vue'
+import ParticipantsForm from '@/modules/MasterData/Participants/components/ParticipantsSearch.vue'
 import { useParticipantsStore } from '@/modules/MasterData/Participants/stores/Participants'
 import { onMounted, ref, watch } from 'vue'
 
 const store = useParticipantsStore()
 
 const search = ref('')
-const showModal = ref(false)
+const showModalSearch = ref(false)
+const showModalAdd    = ref(false)
 
 const kode = ref('')
 const name = ref('')
@@ -21,15 +22,15 @@ const headers = [
   { title: 'Actions', key: 'actions', sortable: false },
 ]
 
-function openShow() {
-  showModal.value = true
+function openshowModalSearch() {
+  showModalSearch.value = true
 }
 
 // Child memilih row
 function setSelected(item) {
   kode.value = item.kode
   name.value = item.name
-  showModal.value = false
+  showModalSearch.value = false
 }
 
 function editItem(item) {
@@ -40,6 +41,10 @@ function editItem(item) {
 function deleteItem(item) {
   console.log('delete =>', item)
   // TODO: panggil API delete
+}
+
+function openshowModalAdd() {
+  showModalAdd.value = true
 }
 
 // WATCH kode -> auto reload datatable
@@ -73,15 +78,10 @@ onMounted(() => store.fetch(''))
 
                 <VCol cols="12" md="8">
                   <VRow align="center">
-                    
+
                     <VCol cols="2">
-                      <VTextField
-                        v-model="kode"
-                        placeholder="Kode Arisan"
-                        density="compact"
-                        variant="outlined"
-                        hide-details
-                      />
+                      <VTextField v-model="kode" placeholder="Kode Arisan" density="compact" variant="outlined"
+                        hide-details />
                     </VCol>
 
                     <VCol cols="1" class="text-center">
@@ -89,18 +89,12 @@ onMounted(() => store.fetch(''))
                     </VCol>
 
                     <VCol cols="4">
-                      <VTextField
-                        v-model="name"
-                        placeholder="Nama Arisan"
-                        density="compact"
-                        variant="outlined"
-                        hide-details
-                        disabled
-                      />
+                      <VTextField v-model="name" placeholder="Nama Arisan" density="compact" variant="outlined"
+                        hide-details disabled />
                     </VCol>
 
                     <VCol cols="2">
-                      <VBtn color="primary" @click="openShow">Search</VBtn>
+                      <VBtn color="primary" @click="openshowModalSearch">Search</VBtn>
                     </VCol>
 
                   </VRow>
@@ -118,31 +112,22 @@ onMounted(() => store.fetch(''))
         <VCard>
           <VCardTitle class="d-flex justify-space-between align-center">
             <span></span>
-
-            <VTextField
-              v-model="search"
-              placeholder="Search..."
-              prepend-inner-icon="ri-search-2-line"
-              variant="solo-filled"
-              density="compact"
-              hide-details
-              style="max-width: 220px"
-            />
+            <div class="d-flex align-center gap-2 w-25">
+              <VTextField v-model="search" placeholder="Search..." prepend-inner-icon="ri-search-2-line"
+                variant="solo-filled" density="compact" hide-details class="flex-grow-1" />
+              <VBtn color="primary" @click="openshowModalAdd">+ Add</VBtn>
+            </div>
           </VCardTitle>
 
-          <VDataTable
-  :headers="headers"
-  :items="store.items"
-  :search="search"
->
-  <template #item.actions="{ item }">
-    <div class="d-flex gap-2">
-      <VBtn size="x-small" color="error" @click="deleteItem(item)">
-        <VIcon icon="ri-delete-bin-6-line" class="mr-1" /> delete
-      </VBtn>
-    </div>
-  </template>
-</VDataTable>
+          <VDataTable :headers="headers" :items="store.items" :search="search">
+            <template #item.actions="{ item }">
+              <div class="d-flex gap-2">
+                <VBtn size="x-small" color="error" @click="deleteItem(item)">
+                  <VIcon icon="ri-delete-bin-6-line" class="mr-1" /> delete
+                </VBtn>
+              </div>
+            </template>
+          </VDataTable>
         </VCard>
       </VCol>
     </VRow>
@@ -150,15 +135,21 @@ onMounted(() => store.fetch(''))
   </div>
 
   <!-- MODAL -->
-  <BaseModalForm v-model="showModal" title="Search Arisan">
+  <BaseModalForm v-model="showModalSearch" title="Search Arisan">
     <ParticipantsForm ref="formRef" @choose="setSelected" />
 
     <template #actions>
-      <VBtn variant="outlined" @click="showModal = false">
+      <VBtn variant="outlined" @click="showModalSearch = false">
         Tutup
       </VBtn>
     </template>
   </BaseModalForm>
+
+  <BaseModalForm v-model="showModalAdd" title="Add Participant">
+    <ParticipantsInputForm @saved="reload" />
+  </BaseModalForm>
+
+
 </template>
 
 <style scoped>
